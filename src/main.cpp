01b900fd687e98404d036ecec10d7d8e1dcdddd3
Grégoire Lefaure <gregoire.lefaure@epita.fr>
2023-02-22 19:35:35 +0000

    Modify main function to allow testing
    Signed-off-by: Grégoire Lefaure <gregoire.lefaure@epita.fr>

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,17 @@
 #include "mainwindow.h"
-
 #include <QApplication>
+
+void startGame() {
+    Board board = new Board();
+    board->printBoard();
+}
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+//    QApplication a(argc, argv);
+//    MainWindow w;
+//    w.show();
+//    return a.exec();
+    startGame();
+    return 0;
 }
