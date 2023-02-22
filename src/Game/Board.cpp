ddc574910a261c77959eaa40956b1073ca9414f6
Grégoire Lefaure <gregoire.lefaure@epita.fr>
2023-02-22 19:26:13 +0000

    First version of Board object done
    Signed-off-by: Grégoire Lefaure <gregoire.lefaure@epita.fr>

--- src/Game/Board.cpp
+++ src/Game/Board.cpp
@@ -1,33 +1,55 @@
 #include "Board.h"
 #include "Cell.h"
 #include "BasePiece.h"
+#include "King.h"
 
 Board::Board() {
-    for(int i = 0; i < m_boardSize * m_boardSize; i++) {
-        m_board[i] = { i };
+    PieceType firstLine = { Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook }
+    
+    int i;
+    
+    // Init black lines
+    for(i = 0; i < m_boardSize; i++) {
+        m_board[i] = new Cell(this, i, firstLine[i], Black);
     }
 
-    // Init pieces too
+    for(; i < 2 * m_boardSize; i++) {
+        m_board[i] = new Cell(this, i, Pawn, Black);
+    }
+
+    // Init empty lines
+    for(; i < (m_boardSize - 2) * m_boardSize; i++) {
+        m_board[i] = new Cell(this, i);
+    }
+
+    // Init white lines
+    for(; i < (m_boardSize - 1) * m_boardSize; i++) {
+        m_board[i] = new Cell(this, i, Pawn, White);
+    }
+
+    for(; i < m_boardSize * m_boardSize; i++) {
+        m_board[i] = new Cell(this, i, firstLine[-(i - 87)], White);
+    }
 }
 
-Cell& Board::getCell(int position) {
+Cell *Board::getCell(int position) {
     return m_board[position];
 }
 
 bool Board::moveCell(int before, int after) {
-    if (m_board[position].state != Busy) {
+    if (m_board[before]->getState() != Busy) {
         return false;
 
     else
-        return m_board[before].piece.move(after);
+        return m_board[before]->getPiece()->move(after);
 }
 
 bool Board::hasWon(ColorPlayer color) {
     ColorPlayer other = color == White ? Black : White;
-    for (Cell cell : m_board) {
-        if (cell.piece.getColor() == other) {
-            if (cell.piece.type == King) {
-                if (cell.piece.possibleMove().empty())
+    for (Cell *cell : m_board) {
+        if (cell->getPiece()->getColor() == other) {
+            if (cell->getPiece()->type == King) {
+                if (cell->getPiece()->possibleMove()->empty())
                     return true;
 
             return false;
@@ -35,4 +57,24 @@ bool Board::hasWon(ColorPlayer color) {
     }
 
     return true;
+}
+
+void Board::printBoard() {
+    for (int i = 0; i < m_boardSize; i++) {
+        for (int j = 0; j < m_boardsize; j++)
+            std::cout << "|---";
+
+        std::cout << "|" << endl;
+
+        for (int j = 0; j < m_boardSize; j++)
+            std::cout << "| " << m_board[i * m_boardSize + j]->getPiece()->type
+                << " ";
+
+        std::cout << "|" << endl;
+    }
+
+    for (int j = 0; j < m_boardsize; j++)
+        std::cout << "|---";
+
+    std::cout << "|" << endl;
 }
--- src/Game/Board.h
+++ src/Game/Board.h
@@ -7,16 +7,17 @@ enum PlayerColor { White, Black };
 class Board {
 private:
     int m_boardSize = 8;
-    Cell m_board[boardSize * boardSize];
+    Cell *m_board[boardSize * boardSize];
 
 public:
     Board();
-    Cell& getCell(int position);
+    Cell *getCell(int position);
     // Return true is the move is done succesfully
     // Check if the move is illegal (outside the bound, not possible with this
     // piece, ...)
     bool moveCell(int before, int after);
     bool hasWon(PlayerColor color);
+    void printBoard();
 };
 
 #endif
