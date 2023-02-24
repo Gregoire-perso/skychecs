99d5a493a191cd779ea6d0ce649b852e5c4a88ac
Grégoire Lefaure <gregoire.lefaure@epita.fr>
2023-02-24 12:37:33 +0000

    possibleMoves done for Knight
    Signed-off-by: Grégoire Lefaure <gregoire.lefaure@epita.fr>

--- src/Game/Pieces/Knight.cpp
+++ src/Game/Pieces/Knight.cpp
@@ -1,6 +1,7 @@
 #include "Knight.h"
 #include "../Board.h"
 #include "../Cell.h"
+#include "../Utils.h"
 
 Knight::Knight(Cell *cell, Board *board, PlayerColor color) 
 : BasePiece(cell, board, color) {
@@ -10,8 +11,42 @@ Knight::Knight(Cell *cell, Board *board, PlayerColor color)
 
 std::vector<int> Knight::possibleMoves() {
     int cur_cell = m_cell->getPosition();
-    int cur_x = cur_cell % Board::boardSize;
-    int cur_y = cur_cell / Board::boardSize;
+    int [cur_x, cur_y] = Utils::to_coord(cur_cell);
     std::vector<int> candidates;
 
+    if (cu_y - 2 >= 0) {
+        if (cur_x - 1 >= 0)
+            candidates.push_back(Utils::to_raw(cur_x - 1, cur_y - 2));
+
+        if (cur_x + 1 < Baord::boardSize)
+            candidates.push_back(Utils::to_raw(cur_x + 1, cur_y - 2));
+    }
+
+    if (cu_y - 1 >= 0) {
+        if (cur_x - 2 >= 0)
+            candidates.push_back(Utils::to_raw(cur_x - 2, cur_y - 1));
+
+        if (cur_x + 2 < Baord::boardSize)
+            candidates.push_back(Utils::to_raw(cur_x + 2, cur_y - 1));
+    }
+
+    if (cu_y + 1 < Board::boardSize) {
+        if (cur_x - 2 >= 0)
+            candidates.push_back(Utils::to_raw(cur_x - 2, cur_y + 1));
+
+        if (cur_x + 2 < Baord::boardSize)
+            candidates.push_back(Utils::to_raw(cur_x + 2, cur_y + 1));
+    }
+
+    if (cu_y + 2 < Board::boardSize) {
+        if (cur_x - 1 >= 0)
+            candidates.push_back(Utils::to_raw(cur_x - 1, cur_y + 2));
+
+        if (cur_x + 1 < Baord::boardSize)
+            candidates.push_back(Utils::to_raw(cur_x + 1, cur_y + 2));
+    }
+
+    pruneCandidates(&candidates);
+
+    return candidates;
 }
