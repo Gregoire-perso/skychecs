2ad0aeb1b55ffa90fd3a294ba1704fc76f6fa9cc
Grégoire Lefaure <gregoire.lefaure@epita.fr>
2023-02-24 10:48:19 +0000

    `possibleMoves' done for Pawn
    Signed-off-by: Grégoire Lefaure <gregoire.lefaure@epita.fr>

--- src/Game/Pieces/Pawn.cpp
+++ src/Game/Pieces/Pawn.cpp
@@ -7,6 +7,13 @@ Pawn::Pawn(Cell *cell, Board *board, PlayerColor color)
     m_type = TPawn;
 }
 
+void Pawn::promoteQueen() {
+
+}
+
+void Pawn::move() {
+
+}
 
 std::vector<int> Pawn::possibleMoves() {
     int cur_cell = m_cell->getPosition();
@@ -14,4 +21,36 @@ std::vector<int> Pawn::possibleMoves() {
     int cur_y = cur_cell / Board::boardSize;
     std::vector<int> candidates;
 
+    int step;
+    if (m_color == Black)
+        step = 1;
+
+    else
+        step = -1;
+
+    if (cur_y + step < 0 || cur_y + step > Board::boardSize) {
+        // Test the cell in front of the pawn
+        Cell *cell = m_board->getCell((cur_y + step) * Board::boardSize + cur_x);
+
+        if (cell->getState() == Busy)
+            if (cell->getPiece()->getColor() != m_color)
+                candidates.push_back((cur_y + step) * Board::boardSize + cur_x);
+        else
+            candidates.push_back((cur_y + step) * Board::boardSize + cur_x);
+
+        // Test the cell in diagonal
+        if (cur_x - 1 >= 0) {
+            *cell = m_board->getCell((cur_y + step) * Board::boardSize + cur_x - 1);
+            if (cell->getState() == Busy && cell->getPiece()->getColor() != m_color)
+                candidates.push_back((cur_y + step) * Board::boardSize + cur_x - 1);
+        }
+
+        if (cur_x + 1 < Board::boardSize) {
+            *cell = m_board->getCell((cur_y + step) * Board::boardSize + cur_x + 1);
+            if (cell->getState() == Busy && cell->getPiece()->getColor() != m_color)
+                candidates.push_back((cur_y + step) * Board::boardSize + cur_x + 1);
+        }
+    }
+
+    return candidates;
 }
